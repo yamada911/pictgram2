1b0e50b8cb0623c4658010a9a9086e5e758d63e9
”名前” <>
2020-04-05 16:29:30 +0000

    rails02完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'pages#index'
   get 'pages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
