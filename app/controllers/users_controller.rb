c67865c89c30f5ac63cc8ee8870742bb7fec7287
”名前” <>
2020-04-05 18:04:18 +0000

    rails04完了 課題は除く

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,18 @@ class UsersController < ApplicationController
   end
   
   def create
-    # binding.pry
-    @user = User.new(name: params[:user][:name], email: params[:user][:email])
+    # binding.pry (name: params[:user][:name], email: params[:user][:email])
+    @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      redirect_to root_path, success: '登録が完了しました'
     else
+      flash.now[:danger] = "登録に失敗しました"
       render :new
     end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
