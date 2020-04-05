c7020a3110e8ff6cd8607890257933f5785be570
”名前” <>
2020-04-05 17:17:26 +0000

    rails03完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'users/new'
+  # rails03 エラー解決の画像 get 'users/new' 記入なし 
   root 'pages#index'
   get 'pages/help'
+  
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
