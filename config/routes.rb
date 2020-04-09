4df9092b23584b8073dd7b7b7a61daa8104a81da
”名前” <>
2020-04-09 09:29:36 +0000

    rails07完了 課題は除く

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,8 @@ Rails.application.routes.draw do
   
   resources :users
   resources :topics
+  
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
