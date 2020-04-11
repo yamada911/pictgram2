eccce7d26de9b611072ab31f0b90b69f468eb768
”名前” <>
2020-04-11 12:12:05 +0000

    rails07完了

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -14,4 +14,11 @@ class FavoritesController < ApplicationController
       redirect_to topics_path, danger: 'お気に入りに登録に失敗しました'
     end
   end
+  
+  # rails07 課題１
+  def destroy
+    @favorite = Favorite.find_by(user_id: current_user.id, topic_id: params[:topic_id])
+    @favorite.destroy
+    redirect_to topics_path, success: 'お気に入り登録を取り消しました'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,6 @@ Rails.application.routes.draw do
   
   get 'favorites/index'
   post '/favorites', to: 'favorites#create'
+  delete '/favorites', to: 'favorites#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
